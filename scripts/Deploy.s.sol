48584c733868bbd96f9c4fcbace593c13fef5264
Josef Vacek <vacekj@outlook.com>
2023-03-23 18:40:14 +0100

    update

--- scripts/Deploy.s.sol
+++ scripts/Deploy.s.sol
@@ -3,11 +3,24 @@ pragma solidity ^0.8.13;
 
 import "../lib/forge-std/src/Script.sol";
 import "../lib/contracts/contracts/votingStrategy/QuadraticFundingStrategy/QuadraticFundingVotingStrategyImplementation.sol";
+import "../lib/contracts/contracts/projectRegistry/ProjectRegistry.sol";
+import "../lib/contracts/contracts/program/ProgramFactory.sol";
+import "../lib/contracts/contracts/program/ProgramImplementation.sol";
 
 contract MyScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
+
+        /* Deploy project registry */
+        ProjectRegistry registry = new ProjectRegistry();
+
+        ProgramFactory programFactory = new ProgramFactory();
+        programFactory.initialize();
+
+        ProgramImplementation programImplementation = new ProgramImplementation();
+        programFactory.updateProgramContract(address(programImplementation));
+
         QuadraticFundingVotingStrategyImplementation qfVotingStratImpl = new QuadraticFundingVotingStrategyImplementation();
         vm.stopBroadcast();
     }
