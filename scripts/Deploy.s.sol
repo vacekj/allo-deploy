fbdf1027c85db7e6d7a7267226d532c1184ad7ed
Josef Vacek <vacekj@outlook.com>
2023-03-23 18:40:14 +0100

    update

--- scripts/Deploy.s.sol
+++ scripts/Deploy.s.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "../lib/forge-std/src//Script.sol";
+import "../lib/forge-std/src/Script.sol";
 import "../lib/contracts/contracts/votingStrategy/QuadraticFundingStrategy/QuadraticFundingVotingStrategyImplementation.sol";
 
 contract MyScript is Script {
     function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
         QuadraticFundingVotingStrategyImplementation qfVotingStratImpl = new QuadraticFundingVotingStrategyImplementation();
-        
+        vm.stopBroadcast();
     }
 }
