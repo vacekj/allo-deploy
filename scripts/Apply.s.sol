dfc7adefe594f5d8ca4b950183ebf16bc3dccaa9
Atris <vacekj@outlook.com>
2023-06-06 21:45:53 +0200

    feat: verify ffi works in between broadcasts

---
+++ scripts/Apply.s.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "../lib/forge-std/src/Script.sol";
+import "../lib/contracts/contracts/votingStrategy/QuadraticFundingStrategy/QuadraticFundingVotingStrategyImplementation.sol";
+import "../lib/contracts/contracts/projectRegistry/ProjectRegistry.sol";
+import "../lib/contracts/contracts/program/ProgramFactory.sol";
+import "../lib/contracts/contracts/program/ProgramImplementation.sol";
+import "../lib/contracts/contracts/votingStrategy/QuadraticFundingStrategy/QuadraticFundingVotingStrategyFactory.sol";
+import "../lib/contracts/contracts/payoutStrategy/MerklePayoutStrategy/MerklePayoutStrategyFactory.sol";
+import "../lib/contracts/contracts/payoutStrategy/MerklePayoutStrategy/MerklePayoutStrategyImplementation.sol";
+import "../lib/contracts/contracts/round/RoundFactory.sol";
+import "../lib/contracts/contracts/round/RoundImplementation.sol";
+import "../lib/contracts/contracts/settings/AlloSettings.sol";
+import "../lib/forge-std/src/Test.sol";
+import "../lib/forge-std/src/console.sol";
+
+contract DeployAllo is Script {
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployerPubKey = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+        vm.startBroadcast(deployerPrivateKey);
+
+        string memory key = "ROUND_ADDRESS";
+        address output = cheats.envAddress(key);
+
+        RoundImplementation roundImpl = RoundImplementation(output);
+        MetaPtr[] memory metaPtrs = new MetaPtr[](4);
+        metaPtrs[0] = MetaPtr(
+            1,
+            "bafybeiekytxwrrfzxvuq3ge5glfzlhkuxjgvx2qb4swodhqd3c3mtc5jay"
+        );
+        metaPtrs[1] = MetaPtr(
+            1,
+            "bafybeih2pise44gkkzj7fdws3knwotppnh4x2gifnbxjtttuv7okw4mjzu"
+        );
+        metaPtrs[2] = MetaPtr(
+            1,
+            "bafybeiceggy6uzfxsn3z6b2rraptp3g2kx2nrwailkjnx522yah43g5tyu"
+        );
+        metaPtrs[3] = MetaPtr(
+            1,
+            "bafybeiceggy6uzfxsn3z6b2rraptp3g2kx2nrwailkjnx522yah43g5tyu"
+        );
+        /* Apply to round with all the projects */
+        for (uint256 i = 0; i < metaPtrs.length; i++) {
+            console.log("Current block time: %s", block.timestamp);
+            round.applyToRound(bytes32(i), metaPtrs[i]);
+        }
+
+        vm.stopBroadcast();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,34 @@ use std::{
 };
 
 mod wait_for_blocks;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
-struct MyData {
+struct Report {
+    transactions: Vec<Transaction>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Transaction {
     hash: String,
     transactionType: String,
     contractName: String,
     contractAddress: String,
     function: Option<String>,
-    arguments: Option<String>,
+    arguments: Option<Vec<String>>,
     rpc: String,
     transaction: TransactionData,
-    additionalContracts: Vec<String>,
     isFixedGasLimit: bool,
+    additionalContracts: Vec<AdditionalContracts>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AdditionalContracts {
+    pub transaction_type: String,
+    pub address: String,
+    pub init_code: String,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -33,25 +47,44 @@ struct TransactionData {
 
 fn main() {
     /* Deploy round and everything  using forge script */
-    let output = Command::new("forge")
+    Command::new("forge")
         .arg("script")
         .arg("scripts/Deploy.s.sol")
         .arg("--broadcast")
-        .arg("-rpc-url http://localhost:8545")
+        .arg("-rpc-url")
+        .arg("http://localhost:8545")
         .arg("-ffi")
         .arg("--via-ir")
-        .output();
-
-    let data = fs::read_to_string("./broadcast/run-latest.json").expect("Unable to read file");
-
-    let json: Vec<MyData> =
-        serde_json::from_str(&data).expect("JSON does not have correct format.");
-
-    dbg!(json);
+        .output()
+        .expect("Failed deploy script");
 
     /* Read round address from latest broadcast, read times from chain */
+    let data = fs::read_to_string("broadcast/Deploy.s.sol/31337/run-latest.json")
+        .expect("Unable to read file");
+
+    let json: Report = serde_json::from_str(&data).expect("JSON does not have correct format.");
+
+    let round_tx = json
+        .transactions
+        .iter()
+        .find(|tx| !tx.additionalContracts.is_empty())
+        .expect("Didn't find addio");
+    let round_address = &round_tx.additionalContracts[0].address;
+
+    /* Pass round address to script */
+    std::env::set_var("ROUND_ADDRESS", round_address);
 
     /* Apply to Round using script */
+    Command::new("forge")
+        .arg("script")
+        .arg("scripts/Apply.s.sol")
+        .arg("--broadcast")
+        .arg("-rpc-url")
+        .arg("http://localhost:8545")
+        .arg("-ffi")
+        .arg("--via-ir")
+        .output()
+        .expect("Failed apply script");
 
     /* Approve/Reject using script */
 
