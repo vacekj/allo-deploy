a6f4133a679c765da7b9faf921e4413d9db724a2
Atris <vacekj@outlook.com>
2023-06-06 21:14:54 +0200

    docs: outline steps

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 mod wait_for_blocks;
 
 fn main() {
-    println!("Hello, world!");
+    /* Deploy round and everything  using forge script */
+
+    /* Read round address from latest broadcast, read times from chain */
+
+    /* Apply to Round using script */
+
+    /* Approve/Reject using script */
+
+    /* Vote using script */
+
+    /* Finalize and payout using script */
 }
